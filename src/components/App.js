44bdd7d9ea195d36975b6bda57eda6785e98a2f8
Aakriti <aakriti1711@gmail.com>
2018-03-20 16:41:25 -0700

    Added data contest & style it with sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 
 class App extends React.Component{
@@ -12,7 +13,12 @@ class App extends React.Component{
             <div className="App">
             <Header message = {this.state.pageHeader}/>
             <div>
-                    %%%
+               {this.props.contests.map(contest => 
+               
+                   <ContestPreview {...contest}/>
+     
+                )}
+                   
             </div>
             </div>
     
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import data from './testData';
+console.log(data);
 
 
 ReactDOM.render(
-    <App />,
+    <App contests ={data.contests}/>,
     document.getElementById('root')
- 
+
 );
