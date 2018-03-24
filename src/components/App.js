b9e4974d1a3d7ca7e5de9b8ad215340bbf0cece9
Aakriti <aakriti1711@gmail.com>
2018-03-23 17:56:13 -0700

    Handle history through window object

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,27 @@ import * as api from '../api';
 
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
-
+const onPopState = handler => {
+  window.onpopstate = handler;
+};
 class App extends React.Component {
   static propTypes ={
     initialData: React.PropTypes.object.isRequired
   };
   state = this.props.initialData;
-  componentDidMount() {
 
+  
+  componentDidMount() {
+    onPopState((event)=> {
+      this.setState({
+        currentContestId:(event.state || {}).currentContestId
+      });
+      
+    });
   }
   componentWillUnmount() {
     // clean timers, listeners
+    onPopState(null);
   }
   fetchContest = (contestId) => {
     pushState(
